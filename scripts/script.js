a6503ac7e61bc0d94f0e80a03b8c0f57fe2a71c0
Domilson Firmino <almeidagilbertofirmino@gmail.com>
2022-11-22 11:19:32 +0000

    signup icons correction

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,19 @@ nosee.addEventListener("click",() =>{
     toggleview.classList.remove("active");
     inputpassword.type = "password";       
 })
+
+//signup
+
+const toggleview2 = document.querySelector('.toggle_view2');
+const see2 = toggleview2.children[1];
+const nosee2 = toggleview2.children[0];
+const inputpassword2 = document.querySelector("#confirm");
+
+see2.addEventListener("click",() =>{
+    toggleview2.classList.add("active");
+    inputpassword2.type = "text";       
+})
+nosee2.addEventListener("click",() =>{
+    toggleview2.classList.remove("active");
+    inputpassword2.type = "password";       
+})
